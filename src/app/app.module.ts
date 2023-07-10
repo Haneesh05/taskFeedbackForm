c04933fe24a9055146f68d41dfac429f56b35019
haneeshhamang <haneeshbandaru@gmail.com>
2023-07-10 07:09:22 +0530

    first commmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
+import { DropdownModule } from 'primeng/dropdown';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackFormComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,FormsModule,
+    AppRoutingModule,ButtonModule,TableModule,DropdownModule,ReactiveFormsModule,BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
